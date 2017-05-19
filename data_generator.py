309bb78c3f5aeb6a4c827d9b86ce0aca446417cd
Curtis Huebner <chuebner@momac33.bccrc.ca>
2017-05-19 11:54:40 -0700

    started work on plot ppd

--- data_generator.py
+++ data_generator.py
@@ -4,38 +4,40 @@ import numpy.random as rnd
 import matplotlib.pyplot as plt
 DIM = 2
 N  = 100
-MAX_AXIS_CLUSTERS = 15
-MAX_CLUSTERS = 15
+MAX_AXIS_CLUSTERS = 5
+MAX_CLUSTERS = 20
 AXIS_ALPHA = 1
 AXIS_BETA = 1
 AXIS_DP_ALPHA = 2
-CLUSTER_DP_ALPHA = 3
-CLUSTER_VARIANCE = 0.01
+CLUSTER_DP_ALPHA = 2
+CLUSTER_CLUSTERING = 500
 
 def generate_data():
     #make axis count prior
     betas = rnd.beta(1,AXIS_DP_ALPHA,size=(DIM,MAX_AXIS_CLUSTERS))
-    axis_cluster_magnitude = np.cumprod(1-betas,axis=1)/(1-betas)*betas
-    axis_cluster_magnitude[:,-1] = 1-np.sum(axis_cluster_magnitude[:,:-1],axis=1)
+    axis_cluster_magnitudes = np.cumprod(1-betas,axis=1)/(1-betas)*betas
+    axis_cluster_magnitudes[:,-1] = 1-np.sum(axis_cluster_magnitudes[:,:-1],axis=1)
 
     axis_cluster_locations = rnd.beta(AXIS_ALPHA,AXIS_BETA,size=(DIM,MAX_AXIS_CLUSTERS))
 
     betas = rnd.beta(1,CLUSTER_DP_ALPHA,size=(MAX_CLUSTERS))
-    cluster_magnitude = np.cumprod(1-betas)/(1-betas)*betas
-    cluster_magnitude[-1] = 1-np.sum(cluster_magnitude[:-1])
+    cluster_magnitudes = np.cumprod(1-betas)/(1-betas)*betas
+    cluster_magnitudes[-1] = 1-np.sum(cluster_magnitudes[:-1])
 
     #spawn axis clusters
     cluster_locations = np.zeros((MAX_CLUSTERS,DIM))
     for dim in range(DIM): 
-        cluster_indicies = rnd.choice(np.arange(MAX_AXIS_CLUSTERS),MAX_CLUSTERS,p=axis_cluster_magnitude[dim,:])
+        cluster_indicies = rnd.choice(np.arange(MAX_AXIS_CLUSTERS),MAX_CLUSTERS,p=axis_cluster_magnitudes[dim,:])
         cluster_locations[:,dim] = axis_cluster_locations[dim,cluster_indicies]
 
     data_expectations = np.zeros((N,DIM))
     for dim in range(DIM): 
-        location_indicies = rnd.choice(np.arange(MAX_CLUSTERS),N,p=cluster_magnitude)
+        location_indicies = rnd.choice(np.arange(MAX_CLUSTERS),N,p=cluster_magnitudes)
         data_expectations[:,dim] = cluster_locations[location_indicies,dim]
-    x = rnd.normal(data_expectations,CLUSTER_VARIANCE)
-    return x,cluster_locations
+        a = CLUSTER_CLUSTERING*data_expectations
+        b = CLUSTER_CLUSTERING*(1-data_expectations)
+    x = rnd.beta(a,b)
+    return x,cluster_locations,cluster_magnitudes,axis_cluster_locations,axis_cluster_magnitudes
 
 if __name__=="__main__":
     x,_ = generate_data()
--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import pymc3 as pm
 import theano as t
 from theano import tensor as tt
+from theano import printing as tp
 from pymc3 import Model
 from pymc3.distributions.continuous import Gamma,Beta
 from pymc3.distributions.discrete import Categorical
@@ -12,9 +13,11 @@ from pymc3 import Deterministic
 from pymc3.backends import Text
 
 from data_generator import generate_data
+from scipy import stats
+import seaborn as sns
 
-MAX_AXIS_CLUSTERS = 15 
-MAX_CLUSTERS = 15
+MAX_AXIS_CLUSTERS = 5 
+MAX_CLUSTERS = 20
 
 def build_model(data):
     n,dim = data.shape
@@ -22,59 +25,97 @@ def build_model(data):
     with bc_model:
         axis_alpha = 1
         axis_beta = 1
-        axis_dp_alpha = Gamma("axis_dp_alpha",mu=1,sd=1)
-        cluster_dp_alpha = Gamma("cluster_dp_alpha",mu=1,sd=1)
-        cluster_std_dev = Gamma("cluster_std_dev",mu=1,sd=1)
+        axis_dp_alpha = 2#Gamma("axis_dp_alpha",mu=2,sd=1)
+        cluster_dp_alpha = 2#Gamma("cluster_dp_alpha",mu=2,sd=1)
+        #how clustered the clusters are, more means tighter clusters
+        cluster_clustering = 500#Gamma("cluster_std_dev",mu=500,sd=250)
         betas = Beta("axis_betas",alpha=axis_alpha,beta=axis_beta,shape=(dim,MAX_AXIS_CLUSTERS))
         axis_cluster_magnitude = tt.extra_ops.cumprod(1-betas,axis=1)/(1-betas)*betas
         axis_cluster_magnitude = tt.set_subtensor(
             axis_cluster_magnitude[:,-1],
             1-tt.sum(axis_cluster_magnitude[:,:-1],axis=1))
-
         axis_cluster_locations = Beta(
             "axis_cluster_locations",alpha=axis_alpha, beta=axis_beta, shape=(dim,MAX_AXIS_CLUSTERS))
 
-        betas = Beta("cluster_betas",1,cluster_dp_alpha,shape=(MAX_CLUSTERS))
-        cluster_magnitude = tt.extra_ops.cumprod((1-betas)/(1-betas)*(betas))
+        betas2 = Beta("cluster_betas",1,cluster_dp_alpha,shape=(MAX_CLUSTERS))
+        cluster_magnitude = tt.extra_ops.cumprod(1-betas2)/(1-betas2)*(betas2)
         cluster_magnitude = tt.set_subtensor(
             cluster_magnitude[-1],
             1-tt.sum(cluster_magnitude[:-1]))
 
+
         #spawn axis clusters
         cluster_locations = tt.zeros((MAX_CLUSTERS,dim))
-        cluster_indicies = Categorical("cluster_indicies",shape=(MAX_CLUSTERS),p=axis_cluster_magnitude.T)
+        #one = tp.Print()(tt.sum(axis_cluster_magnitude,axis=1))
+        #axis_cluster_magnitude=axis_cluster_magnitude*tt.sum(one/2)
+        cluster_indicies = Categorical("cluster_indicies",shape=(MAX_CLUSTERS,dim),p=axis_cluster_magnitude)
         for d in range(dim):
             #TODO:find a cleaner way of doing this
             cluster_locations = tt.set_subtensor(
                 cluster_locations[:,d],
-                axis_cluster_locations[d,cluster_indicies])
+                axis_cluster_locations[d,cluster_indicies[:,d]])
 
         loc = Deterministic("cluster_locations",cluster_locations)
+        loc = Deterministic("cluster_magnitudes",cluster_magnitude)
 
         data_expectation = tt.zeros((n,dim))
+        #one = tp.Print()(tt.sum(cluster_magnitude))
+        #cluster_magnitude=cluster_magnitude*one
         location_indicies = Categorical("location_indicies",shape=(n),p=cluster_magnitude)
         for d in range(dim):
             data_expectation = tt.set_subtensor(
                 data_expectation[:,d],
                 cluster_locations[location_indicies,d])
-
-        x = Beta("data".format(d),shape=(n,d),mu=data_expectation,sd=cluster_std_dev,observed=data)
+        #x = Beta("data".format(d),shape=(n,d),mu=data_expectation,sd=cluster_std_dev,observed=data)
+        a=data_expectation*cluster_clustering
+        b=(1-data_expectation)*cluster_clustering
+        x = Beta("data",shape=(n,dim),alpha=a,beta=b,observed=data)
         db = Text('trace')
-        trace = pm.sample(100000, n_init=500, njobs=5 ,trace=db)
-        print(list(trace["cluster_locations"]))
-    return bc_model
+        
+        steps1 = pm.CategoricalGibbsMetropolis(vars=[location_indicies],proposal='uniform')
+        steps2 = pm.CategoricalGibbsMetropolis(vars=[cluster_indicies],proposal='uniform')
+        steps3 = pm.step_methods.HamiltonianMC(vars=[betas,betas2,axis_cluster_locations],step_scale=0.02,path_length=1)
+        #steps2 = pm.Metropolis(vars=[cluster_B])
+        trace = pm.sample(3000,tune=40000,n_init=10000, njobs=1,step=[steps1,steps2,steps3]) #,trace=db)
 
+    return bc_model,trace
+    
+def get_map_trace(trace,model):
+    pass
+
+def plot_posterior_predictive(trace,data):
+    samples = pm.sample_ppc(trace,len(trace)*10,)
+    pass
+    
+    
+
+def plot_max_n(trace,n,last,spacing):
+    cl = trace["cluster_locations"]
+    cm = trace["cluster_magnitudes"]
+    print(cl)
+
+    #find indicies of the largest clusters
+    sort = np.argsort(cm,axis=1)
+    #biggest cluster
+    for i in range(n):
+        indicies = sort[-last::spacing,i]
+        values = cl[-last::spacing,indicies,:]
+        sns.set_style('whitegrid')
+        sns.plt.ylim(0,1)
+        sns.plt.xlim(0,1)
+        sns.kdeplot(values[:,0],values[:,0], bw='scott')
+def infer_parameters(model):
+    pass
 
 def main():
-    data,locations = generate_data()
-    model = build_model(data)
+    data,locations,mag,_,_ = generate_data()
+    model,trace = build_model(data)
+    plot_ppd(trace,data)
+    sns.regplot(x=data[:,0], y=data[:,1], marker="+",fit_reg=False)
     parameters = infer_parameters(model)
-    print(locations)
+    plt.show()
 
 if __name__=="__main__":
-    #x = generate_data()
-    #plt.plot(x[:,0],x[:,1],"o")
-    #plt.show()
     print("START")
     main()
     print("DONE")
